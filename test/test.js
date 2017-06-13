56a567cec06e19392b1823dfd24d59e4ff59297f
Parham Alvani <parham.alvani@gmail.com>
2017-06-13 22:20:59 +0430

    Adds test sample for pipe.

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const child_process = require("child_process");
 
 
 loole.createFifo('/tmp/Hello').then(() => {
-  console.log('we have a new and ready pipe.');
+  console.log('we have a new and ready fifo.');
 
   const cp1 = child_process.spawn('ls', ['-la']);
   cp1.stdout.pipe(fs.createWriteStream('/tmp/Hello'));
@@ -14,4 +14,14 @@ loole.createFifo('/tmp/Hello').then(() => {
   cp2.stdout.on('end', () => {
     loole.unlinkFifo('/tmp/Hello');
   });
+});
+
+loole.createPipe().then((fd1, fd2) => {
+  console.log('we have a new and ready pipe.');
+
+  const cp1 = child_process.spawn('ls', ['-la', '/dev']);
+  cp1.stdout.pipe(fs.createWriteStream(`/dev/fd/${fd1}`));
+
+  const cp2 = child_process.spawn('cat', [`/dev/fd/${fd2}`]);
+  cp2.stdout.pipe(process.stdout);
 });
